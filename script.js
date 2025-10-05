8a3a31831e2be1aaec9c42ecacb12f742f41a139
systemprogramming24-dot <systemprogramming24@gmail.com>
2025-10-05 18:46:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const proxy = "https://corsproxy.io/?";
-const url = proxy + "https://script.google.com/macros/s/AKfycbzjyES7ZSOrsIyavG8ut7QskCmZorr7L-FkVigZj74lCD80Of95AtyxkGIsnj4o3My0JA/exec";
-
 fetch(url, {
   method: "POST",
   body: JSON.stringify({ name: guestName, validateOnly: true }),
   headers: { "Content-Type": "application/json" }
 })
-.then(res => res.json())
-.then(data => {
-  console.log("Respuesta:", data);
+.then(res => res.text())
+.then(text => {
+  console.log("Texto bruto:", text);
+  try {
+    const json = JSON.parse(text);
+    console.log("JSON parseado:", json);
+  } catch (e) {
+    console.error("No se pudo parsear como JSON:", e);
+  }
 })
-.catch(err => console.error("Error:", err));
+.catch(err => console.error("Error de red:", err));
