2c20249b1ba59f4c40fd5e6c271d6e50bd68b50c
systemprogramming24-dot <systemprogramming24@gmail.com>
2025-10-05 17:40:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ guestNameDisplay.textContent = guestName || 'Invitado';
 nameInput.value = guestName;
 
 // Verificar si está en la lista
-fetch('https://script.google.com/macros/s/AKfycbx_Ty9mbwv7564q55dLCFcm68QuIbxZv6jaKqa773oqm3QxiO5ip8WLvnYOmI7U4HupTQ/exec', {
+fetch('https://script.google.com/macros/s/AKfycbxo5ART8l3_0Zc_AfNLhb8rrb_4ud50V4JftzRrawuax-afIc-hFvNGCMffGwjcG_-Gqw/exec', {
   method: 'POST',
   body: JSON.stringify({ name: guestName, validateOnly: true }),
   headers: { 'Content-Type': 'text/plain' }
@@ -63,7 +63,7 @@ function sendConfirmation(response) {
     guests: response === "Sí" ? document.getElementById('guests').value || 1 : 0
   };
 
-  fetch('https://script.google.com/macros/s/AKfycbx_Ty9mbwv7564q55dLCFcm68QuIbxZv6jaKqa773oqm3QxiO5ip8WLvnYOmI7U4HupTQ/exec', {
+  fetch('https://script.google.com/macros/s/AKfycbxo5ART8l3_0Zc_AfNLhb8rrb_4ud50V4JftzRrawuax-afIc-hFvNGCMffGwjcG_-Gqw/exec', {
     method: 'POST',
     body: JSON.stringify(data),
     headers: { 'Content-Type': 'text/plain' } 
@@ -75,6 +75,7 @@ function sendConfirmation(response) {
   })
   .catch(err => alert('Error al enviar confirmación.'));
 }
+
 
 
 
